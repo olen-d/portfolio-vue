4ab2598e9d3b4002116d498501a08ed13d7f2b42
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-10-10 02:17:05 -0400

    Edit the welcome headline from the admin interface

---
+++ app/models/updateHeadline.js
@@ -0,0 +1,32 @@
+const db = require("../config/connection");
+const ObjectID = require("mongodb").ObjectID;
+// TODO: Add CreatedAt and UpdatedAt fields...
+const data = headlineData => {
+  return new Promise((resolve, reject) => {
+    try {
+      let id = ObjectID(headlineData.id);
+      db.welcome.update(
+        {
+          _id: id
+        },
+        {
+          $set: { headline: headlineData.headline }
+        },
+        (error, response) => {
+          if (error) {
+            resolve(error);
+          } else {
+            resolve(response);
+          }
+        }
+      );
+    } catch (err) {
+      reject({
+        status: 500,
+        error: "Internal server error. Failed to create new user."
+      });
+    }
+  });
+};
+
+module.exports = { data: data };
