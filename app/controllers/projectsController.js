1687e4d2fc98dd1be22bf4b16441e05431a80353
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-11-06 01:22:47 -0500

    Interim save. Multer working correctly

--- app/controllers/projectsController.js
+++ app/controllers/projectsController.js
@@ -2,39 +2,24 @@
 const createProject = require("../models/createProject");
 const readProjects = require("../models/readProjects");
 
-// Middleware
-const multer = require("multer");
-
 // Helpers
 const auth = require("../helpers/auth-module");
 
-// Set Storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/assets/images");
-  }, // TODO - FIX THIS TO FILTER FOR IMAGE FILES
-  filename: (req, file, cb) => {
-    cb(null, "fsf" + "-" + Date.now() + "-" + file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
-
-(exports.create_project = upload.single("file")),
-  (req, res) => {
-    auth
-      .checkAuth(req.headers)
-      .then(response => {
-        if (response.auth && response.administrator) {
-          console.log(req.body);
-        } else {
-          // Not Authorized
-        }
-      })
-      .catch(err => {
-        // Err
-      });
-  };
+exports.create_project = (req, res) => {
+  auth
+    .checkAuth(req.headers)
+    .then(response => {
+      if (response.auth && response.administrator) {
+        console.log("NINJAS\n", req.body);
+      } else {
+        // Not Authorized
+        // TODO: Remove the uploaded file and kick out an error.
+      }
+    })
+    .catch(err => {
+      // Err
+    });
+};
 
 exports.read_projects = (req, res) => {
   readProjects
--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+// Middleware
+const multer = require("multer");
+
+// Set Storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets/images");
+  }, // TODO - FIX THIS TO FILTER FOR IMAGE FILES
+  filename: (req, file, cb) => {
+    cb(null, "fsf" + "-" + Date.now() + "-" + file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
 const projects_controller = require("../controllers/projectsController");
 
-router.get("/projects", projects_controller.read_projects);
+router.post(
+  "/projects/create",
+  upload.single("file"),
+  projects_controller.create_project
+);
 
-router.post("/projects/create", projects_controller.create_project);
+router.get("/projects", projects_controller.read_projects);
 
 module.exports = router;
