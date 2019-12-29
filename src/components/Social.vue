4e21c8b477ddfd879ca8ddfcd8df488ff03f5295
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-12-29 17:47:16 -0500

    Loading indicator working for social media fetch call

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,29 +1,87 @@
 <template>
-  <div id="social">
-    <span v-for="{ _id, text, href, icon } in social" :key="_id" class="social-items">
-      <a :href="href" :title="`Olen Daelhousen on ${text}`"><i :class="icon"></i></a>&nbsp;
-    </span>
+  <div class="social-wrapper">
+    <div v-if="loading || error" class="loading-indicator-wrapper">
+      <LoadingIndicator v-bind:loading="loading" v-bind:error="error" />
+    </div>
+    <div v-if="social" id="social">
+      <span
+        v-for="{ _id, text, href, icon } in social"
+        :key="_id"
+        class="social-items"
+      >
+        <a :href="href" :title="`Olen Daelhousen on ${text}`">
+          <i :class="icon"></i>
+        </a>&nbsp;
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-module.exports = {
+import LoadingIndicator from "./LoadingIndicator.vue";
+
+export default {
+  components: {
+    LoadingIndicator
+  },
 
   data: () => {
     return {
-      social: []
-    }
+      loading: false,
+      social: null,
+      error: null
+    };
   },
 
   created() {
+    this.loading = true;
     fetch(`${process.env.VUE_APP_API_BASE_URL}/api/social/user/olen.d`)
-      .then((response) => {
-        return response.json();
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Network response was not ok. Unable to fetch. ");
+        }
       })
-      .then((json) => {
+      .then(json => {
+        this.loading = false;
         this.social = json.social;
+      })
+      .catch(err => {
+        this.loading = false;
+        this.error = err.toString();
       });
   }
-
-}
+};
 </script>
+
+<style scoped>
+.loading-indicator-wrapper {
+  width: 60%;
+  margin: 2.225rem auto 2.225rem auto;
+}
+
+.social-wrapper {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  background-color: #136a9b;
+  color: #fcc914;
+  text-align: center;
+}
+
+.social-items {
+  font-size: 4rem;
+}
+
+.social-wrapper a:link {
+  color: #ffffff;
+}
+
+.social-wrapper a:visited {
+  color: #ffffff;
+}
+
+.social-wrapper a:hover {
+  color: #fcc914;
+}
+</style>
