dde8c9807027815853de31cd70e5debd0c47a7aa
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-05-27 01:01:17 -0400

    Step 1 of Strava API Authorization complete

--- app/models/readActivities.js
+++ app/models/readActivities.js
@@ -6,16 +6,19 @@ const apiUrl = "https://www.strava.com/api";
 const apiVersion = "v3";
 
 // TODO: Move the following to actual authentication when completed
-const accessToken = "f9f9e7f478036ec67f20631640efb81508067d5b";
+const accessToken = "7a8185ef35055627298f3d4ecc129d7735955114";
 const userId = "28169480";
 //athletes/28169480/?access_token=f9f9e7f478036ec67f20631640efb81508067d5b
+// `${apiUrl}/${apiVersion}/athletes/${userId}/?access_token=${accessToken}`
 
 // Access the Strava API
 const fetchActivities = () => {
   return new Promise((resolve, reject) => {
-    fetch(
-      `${apiUrl}/${apiVersion}/athletes/${userId}/?access_token=${accessToken}`
-    )
+    fetch(`${apiUrl}/${apiVersion}/activities`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
       .then(data => {
         data.json().then(json => {
           resolve(json);
---
+++ src/components/AuthorizeStrava.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="front-end">
+    <Header></Header>
+    <div id="about" class="container">
+      <div class="row">
+        <div class="one column">
+          &nbsp;
+        </div>
+        <div class="ten columns">
+          Doritos
+        </div>
+        <div class="one column">
+          &nbsp;
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    Header
+  },
+
+  created() {
+    const stravaAuthorizeURI = process.env.VUE_APP_STRAVA_AUTHORIZE_URI;
+    const stravaClientId = process.env.VUE_APP_STRAVA_CLIENT_ID;
+    const stravaRedirectURI = process.env.VUE_APP_STRAVA_REDIRECT_URI;
+
+    window.location = `${stravaAuthorizeURI}?client_id=${stravaClientId}&redirect_uri=${stravaRedirectURI}&response_type=code&scope=activity:read`;
+  }
+};
+</script>
