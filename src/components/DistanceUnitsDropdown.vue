d2865de0716217de450c90040583e870a78a952a
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-06-24 01:05:09 -0400

    Created a units dropdown for the activity graph

---
+++ src/components/DistanceUnitsDropdown.vue
@@ -0,0 +1,30 @@
+<template>
+  <select name="distance-units" v-model="distanceUnit" @change="onChange">
+    <option v-for="{ _id, name } in units" :key="_id" :value="_id">
+      {{ name }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    defaultUnits: Number
+  },
+
+  data: function() {
+    return {
+      units: [{ _id: 0, name: "miles" }, { _id: 1, name: "kilometers" }],
+      distanceUnit: this.defaultUnits
+    };
+  },
+
+  methods: {
+    onChange(event) {
+      this.$emit("use-distance-units", this.distanceUnit);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
