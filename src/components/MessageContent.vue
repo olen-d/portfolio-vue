f25e764d3a35835f4c20437b8624aeff8c88ff52
Salvador Dieffenbach <zogwarg@hotmail.com>
2021-02-15 21:56:06 -0500

    Message component that displays a header and content.

---
+++ src/components/MessageContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="message-content" v-if="messageContent.length > 0">
+    <p
+      class="message-content-item"
+      v-for="item in messageContent"
+      v-bind:key="item.slice(0, 10)"
+    >
+      {{ item }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    messageContent: {
+      type: Array
+    }
+  }
+};
+</script>
---
+++ src/components/MessageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="message-header">
+    <p>
+      <b>{{ messageHeader }}</b>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    messageHeader: {
+      default: null,
+      type: String
+    }
+  }
+}
+</script>
