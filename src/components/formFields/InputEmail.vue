abee89a0c3a65997dd0a205e38a0410840afc941
olen-d <olen.daelhousen@outlook.com>
2022-10-05 23:55:27 -0400

    Refactor InputEmail to use Vue 3 Composition API

--- src/components/formFields/InputEmail.vue
+++ src/components/formFields/InputEmail.vue
@@ -1,82 +1,93 @@
+<script setup>
+  import { onMounted, ref, watch } from 'vue'
+
+  const props = defineProps({
+    initialValue: {
+      type: String,
+      default: ''
+    },
+    isServerError: {
+      type: Boolean,
+      default: false
+    },
+    labeltext: {
+      type: String,
+      default: 'Email Address'
+    },
+    placeholder: {
+      type: String,
+      default: 'Enter your email address...'
+    },
+    required: {
+      type: Boolean,
+      default: false
+    }
+  })
+
+  const emit = defineEmits(['changeFormValues'])
+
+  const errorMessage = 'Please enter a valid email address'
+  const isValid = ref(false)
+  const emailAddress = ref('')
+  const validationStatus = ref('')
+
+  onMounted(() => {
+   emailAddress.value = props.initialValue
+    emit('changeFormValues', { inputName: 'emailAddress', inputValue: emailAddress.value, isValid: isValid.value, errorMessage })
+  })
+
+  const checkMx = async emailAddress => {
+    try {
+      const result = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/api/mail/check-mx/${email}`
+      );
+      const data = await result.json();
+      const { mxExists } = data;
+
+      return mxExists ? true : false;
+    } catch {
+      return false;
+    }
+  }
+
+  const handleBlur = async () => {
+    isValid.value = await validate(emailAddress.value)
+    validationStatus.value = isValid.value ? null : 'error'
+    emit('changeFormValues', { inputName: 'emailAddress', inputValue: emailAddress.value, isValid: isValid.value, errorMessage })
+  }
+
+  const validate = async emailAddress => {
+    const expression = /.+@.+\..+/i
+    const isValidFormat = expression.test(String(emailAddress).toLowerCase())
+    if (isValidFormat) {
+      return await checkMx(emailAddress)
+    } else {
+      return false
+    }
+  }
+
+  watch(() => props.isServerError, (isServerError, prevIsServerError) => {
+    if (isServerError) {
+      validationStatus.value = 'error'
+      emit('changeFormValues', { inputName: 'emailAddress', inputValue: emailAddress.value, isValid: false, errorMessage })
+    }
+  })
+
+</script>
+
 <template>
   <div class="input-email">
-    <label for="email" v-bind:class="{ 'text-error': isError }">
-      {{ label }}
+    <label for="email" v-bind:class="{ 'text-error': !isValid }">
+      {{ labeltext }}
     </label>
     <input
-      v-model="inputData.email"
-      v-on:blur="handleBlur"
+      v-model="emailAddress"
       type="email"
       class="u-full-width"
       id="email"
-      v-bind:placeholder="placeholder"
-      required
+      :placeholder="placeholder"
+      :required="required"
+      @blur="handleBlur"
     />
   </div>
 </template>
-
-<script>
-export default {
-  props: {
-    label: {
-      default: "Email Address",
-      type: String
-    },
-    method: { type: Function },
-    placeholder: {
-      default: "Your Email Address",
-      type: String
-    }
-  },
-
-  data: () => {
-    return {
-      inputData: { email: "" },
-      isError: false
-    };
-  },
-
-  methods: {
-    async handleBlur() {
-      const isValid = await this.validate();
-      if (!isValid) {
-        this.$store.commit("setStatusCategory", "error");
-        this.$store.commit(
-          "setStatusMessage",
-          "Invalid email. Please enter a valid email address in the format: name@example.com and try again."
-        );
-        this.isError = true;
-      } else {
-        this.$store.commit("setStatusCategory", "");
-        this.$store.commit("setStatusMessage", "");
-        this.isError = false;
-      }
-
-      this.$emit("send-email-value", "blur", this.inputData.email);
-    },
-
-    async validate() {
-      const expression = /.+@.+\..+/i;
-      const {
-        inputData: { email }
-      } = this;
-
-      if (expression.test(String(email).toLowerCase())) {
-        try {
-          const result = await fetch(
-            `${process.env.VUE_APP_API_BASE_URL}/api/mail/check-mx/${email}`
-          );
-          const data = await result.json();
-          const { mxExists } = data;
-
-          return mxExists ? true : false;
-        } catch {
-          return false;
-        }
-      } else {
-        return false;
-      }
-    }
-  }
-};
-</script>
