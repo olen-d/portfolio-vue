47ac5da3656747ad1a21b8f335ec81bd496cf4b2
olen-d <olen.daelhousen@outlook.com>
2022-11-15 18:30:58 -0500

    Refactor Header Front End to use Vue 3 composition API

--- src/components/HeaderFrontEnd.vue
+++ src/components/HeaderFrontEnd.vue
@@ -1,29 +1,26 @@
+<script setup>
+  import { onMounted, ref } from 'vue'
+
+  const viteHostURL = ref(null)
+
+  const returnImageSource = image => {
+    return `${import.meta.env.VITE_ASSETS_HOST}${image}`
+  }
+
+  onMounted(() => {
+   viteHostURL.value = import.meta.env.VITE_ASSETS_HOST
+  })
+</script>
+
 <template>
-  <div class="logo-header" v-if="viteHostURL">
+  <div class="header-front-end" v-if="viteHostURL">
     <img :src="returnImageSource('/assets/images/OD_min_500x500.png')" width="200" height="200" />
   </div>
 </template>
 
-<script>
-export default {
-  data: () => {
-    return {
-      viteHostURL: null
-    }
-  },
-
-  methods: {
-    returnImageSource(image) {
-      return `${import.meta.env.VITE_ASSETS_HOST}${image}`
-    }
-  },
-
-  created () {
-    this.viteHostURL = import.meta.env.VITE_ASSETS_HOST
-  }
-}
-</script>
-
 <style scoped>
-
+.header-front-end {
+    margin-top:-20rem;
+    margin-left:5rem;
+}
 </style>
