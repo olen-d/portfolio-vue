7879afb6b5850da97bc9f22dc9d0385e8c918dc4
olen-d <olen.daelhousen@outlook.com>
2022-11-17 00:50:36 -0500

    Refactor Statistics Units Dropdown Component to use Vue 3 composition API

--- src/components/StatisticsUnitsDropdown.vue
+++ src/components/StatisticsUnitsDropdown.vue
@@ -1,3 +1,61 @@
+<script setup>
+  import { computed, onMounted, ref, watch } from 'vue'
+
+  const emits = defineEmits(['useStatisticsUnits'])
+  const props = defineProps({
+    activityStatistic: {
+      type: String
+    },
+    defaultStatisticUnit: {
+      type: String
+    }
+  })
+
+  const statisticsUnit = ref('')
+
+  onMounted(() => {
+    statisticsUnit.value = props.defaultStatisticUnit
+  })
+
+  const statisticsUnitsOptions = computed(() => {
+    if (props.activityStatistic === "distance") {
+      return [
+        { _id: "mi", name: "miles" },
+        { _id: "km", name: "kilometers" }
+      ];
+    } else if (props.activityStatistic === "movingTime") {
+      return [{ _id: "elapsedTime", name: "elapsed time " }];
+    } else if (props.activityStatistic === "elevationGain") {
+      return [{ _id: "ft", name: "feet" }, { _id: "m", name: "meters" }];
+    } else if (props.activityStatistic === "sufferScore") {
+      return [{ _id: "suffering", name: "suffering units" }];
+    } else if (props.activityStatistic === "averageSpeed") {
+      return [
+        { _id: "mph", name: "miles per hour" },
+        { _id: "kph", name: "kilometers per hour" }
+      ];
+    } else {
+      return "error"
+    }
+  })
+
+  const onChange = () => {
+    emits('useStatisticsUnits', statisticsUnit.value)
+  }
+  watch(() => props.updatedLinkData, (newUpdatedLinkData, prevUpdatedLinkData) => {
+    if (newUpdatedLinkData !== prevUpdatedLinkData) {
+      updateSocial()
+    }
+  })
+
+  // Force the units to change when the prop is changed in the parent.
+  watch(() => props.defaultStatisticUnit, (newDefaultStatisticUnit, prevDefaultStatisticUnit) => {
+    if (newDefaultStatisticUnit !== prevDefaultStatisticUnit) {
+      statisticsUnit.value = props.defaultStatisticUnit
+    }
+  })
+</script>
+
 <template>
   <span class="statistics-units">
     &nbsp;
@@ -13,58 +71,3 @@
     &nbsp;
   </span>
 </template>
-
-<script>
-export default {
-  props: {
-    activityStatistic: String,
-    defaultStatisticUnit: String
-  },
-
-  data: function() {
-    return {
-      statisticsUnit: this.defaultStatisticUnit
-    };
-  },
-
-  computed: {
-    statisticsUnitsOptions: function() {
-      const { activityStatistic } = this;
-      if (activityStatistic === "distance") {
-        return [
-          { _id: "mi", name: "miles" },
-          { _id: "km", name: "kilometers" }
-        ];
-      } else if (activityStatistic === "movingTime") {
-        return [{ _id: "elapsedTime", name: "elapsed time " }];
-      } else if (activityStatistic === "elevationGain") {
-        return [{ _id: "ft", name: "feet" }, { _id: "m", name: "meters" }];
-      } else if (activityStatistic === "sufferScore") {
-        return [{ _id: "suffering", name: "suffering units" }];
-      } else if (activityStatistic === "averageSpeed") {
-        return [
-          { _id: "mph", name: "miles per hour" },
-          { _id: "kph", name: "kilometers per hour" }
-        ];
-      } else {
-        return "error";
-      }
-    }
-  },
-
-  methods: {
-    onChange() {
-      this.$emit("use-statistics-units", this.statisticsUnit);
-    }
-  },
-
-  // Force the units to change when the prop is changed in the parent.
-  watch: {
-    defaultStatisticUnit: function() {
-      this.statisticsUnit = this.defaultStatisticUnit;
-    }
-  }
-};
-</script>
-
-<style scoped></style>
