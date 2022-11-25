3153deeefe453c957703a9ae5b27dbc98b6357fb
olen-d <olen.daelhousen@outlook.com>
2022-11-25 00:18:05 -0500

    Refactor Contact component to split contact information and form. Rename to ContactInformation.

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,9 +1,30 @@
 <script setup>
-  import Contact from '@/components/Contact.vue'
+  import ContactInformation from '@/components/ContactInformation.vue'
+  import FormContact from '@/components/FormContact.vue'
+  import HeaderFrontEnd from '@/components/HeaderFrontEnd.vue'
 </script>
 
 <template>
   <div class="contact-page">
-    <Contact />
+    <div class="front-end">
+      <HeaderFrontEnd />
+      <div class="container">
+        <div class="row">
+          <div class="one column">
+            &nbsp;
+          </div>
+          <div class="ten columns">
+            <h1>
+              Get in Touch
+            </h1>
+            <ContactInformation />
+          </div>
+          <div class="one column">
+            &nbsp;
+          </div>
+        </div>
+        <FormContact formName="Send Us a Message" />
+      </div>
+    </div>
   </div>
 </template>
