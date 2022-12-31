7bfd76a616929da306f8a19921174f0c9f98f9f7
olen-d <olen.daelhousen@outlook.com>
2022-12-31 13:24:35 -0500

    Implement basic breadcrumbs on admin side

---
+++ src/components/AdminBreadCrumbs.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  breadcrumbs: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="admin-bread-crumbs">
+    {{ breadcrumbs.join(' / ') }}
+  </div>
+</template>
